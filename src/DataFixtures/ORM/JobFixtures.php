9c9df32df68d3dad07f857eaa18ce7cca94568a7
Dexter <josja_benard@hotmail.com>
2018-03-30 17:38:04 +0200

    Fixtures. - Job names now more realistic (and sometimes funny).

--- src/DataFixtures/ORM/JobFixtures.php
+++ src/DataFixtures/ORM/JobFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures\ORM;
 
+use App\Entity\Job;
 use App\Entity\Pattern;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -9,6 +10,68 @@ use Nelmio\Alice\Loader\NativeLoader;
 
 class JobFixtures extends Fixture
 {
+    private static $jobPrefixes = ['sr.', 'jr.', 'supervisor', 'Lead', 'hoofd', 'assistent', 'allround'];
+
+    private static $jobNames = [
+        'Aannemer',
+        'Aartsbisschop',
+        'Accountant',
+        'Acrobaat',
+        'Acteur',
+        'Activiteitenbegeleider',
+        'Chefkok',
+        'Chemicus',
+        'Chirurg',
+        'Clown',
+        'Componist',
+        'Computerprogrammeur',
+        'Conciërge',
+        'Conducteur',
+        'Edelsmid',
+        'Elektromonteur',
+        'Etaleur',
+        'Fietsenmaker',
+        'Fijnbankwerker',
+        'Fotograaf',
+        'Fruitteler',
+        'Fysicus',
+        'Garnalenpeller',
+        'Gemeentesecretaris',
+        'Generaal',
+        'Geograaf',
+        'Geoloo',
+        'IJzervlechter',
+        'Illusionist',
+        'Imker',
+        'Komiek',
+        'Koster',
+        'Kostuumontwerper',
+        'Kraamverzorger',
+        'Naaister',
+        'Nachtwaker',
+        'Fysicus',
+        'Neuroloog',
+        'Nieuwslezer',
+        'Pijpfitter',
+        'Piloot',
+        'Poelier',
+        'Poffertjesbakker',
+        'Politieagent',
+        'Portier',
+        'Postbode',
+        'Chemicus',
+        'Schipper',
+        'Schoenmaker',
+        'Schooldecaan',
+        'Veehouder',
+        'Verhuizer',
+        'Verkoper',
+        'Verloskundige',
+        'Verpleegkundige',
+        'Zeefdrukker',
+        'Zilversmid',
+    ];
+
     private static $indicatorsData = [
         ['name' => 'a', 'range_low' => 0, 'range_high' => 1000],
         ['name' => 'b', 'range_low' => 0, 'range_high' => 1000],
@@ -43,6 +106,8 @@ class JobFixtures extends Fixture
     {
         if ($object instanceof Pattern) {
             $this->handlePattern($object);
+        } elseif ($object instanceof Job) {
+            $this->setName($object);
         }
     }
 
@@ -52,6 +117,26 @@ class JobFixtures extends Fixture
     private function handlePattern(Pattern $pattern): void
     {
         $pattern->setParameters($this->generateParameters());
+    }
+
+    /**
+     * @param Job $job
+     */
+    private function setName(Job $job): void
+    {
+        $rand1 = rand(0, 9);
+        $rand2 = rand(0, 9);
+        $name = '';
+        if ($rand1 > 3) {
+            $name .= self::$jobPrefixes[array_rand(self::$jobPrefixes)].' ';
+        }
+
+        if ($rand2 > 3) {
+            $name .= $job->getName().' ';
+        }
+
+        $name .= self::$jobNames[array_rand(self::$jobNames)];
+        $job->setName($name);
     }
 
     /**
