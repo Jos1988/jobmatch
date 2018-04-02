9637919dbccc4ad45d2c5e3233602b0e090b5da3
Dexter <josja_benard@hotmail.com>
2018-04-02 11:14:13 +0200

    Frontend - working on status page todo list.

---
+++ src/Entity/Project/Todo.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace App\Entity\Project;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\Project\ProjectTodoRepository")
+ * @ORM\Table(name="project_todo")
+ */
+class Todo
+{
+
+    const todo = 'TODO';
+    const onIt = 'ON_IT';
+    const complete = 'COMPLETE';
+    const wontDo = 'WONT_DO';
+
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=10, options={"default"="TODO"})
+     */
+    private $status = self::todo;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string")
+     */
+    private $action;
+
+    /**
+     * @var string
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    /**
+     * DateTime of last status change.
+     *
+     * @var \DateTime
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
+
+    /**
+     * get Id
+     *
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * set Id
+     *
+     * @param mixed $id
+     *
+     * @return $this
+     */
+    public function setId($id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * get Status
+     *
+     * @return string
+     */
+    public function getStatus(): string
+    {
+        return $this->status;
+    }
+
+    /**
+     * set Status
+     *
+     * @param string $status
+     *
+     * @return $this
+     */
+    public function setStatus(string $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * get Action
+     *
+     * @return string
+     */
+    public function getAction(): string
+    {
+        return $this->action;
+    }
+
+    /**
+     * set Action
+     *
+     * @param string $action
+     *
+     * @return $this
+     */
+    public function setAction(string $action): self
+    {
+        $this->action = $action;
+
+        return $this;
+    }
+
+    /**
+     * get Description
+     *
+     * @return string
+     */
+    public function getDescription(): string
+    {
+        return $this->description;
+    }
+
+    /**
+     * set Description
+     *
+     * @param string $description
+     *
+     * @return $this
+     */
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * get Date
+     *
+     * @return \DateTime
+     */
+    public function getDate(): \DateTime
+    {
+        return $this->date;
+    }
+
+    /**
+     * set Date
+     *
+     * @param \DateTime $date
+     *
+     * @return $this
+     */
+    public function setDate(\DateTime $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+}
